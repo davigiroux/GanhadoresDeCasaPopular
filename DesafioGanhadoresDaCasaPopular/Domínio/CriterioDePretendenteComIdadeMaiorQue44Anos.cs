42fb06bbd09b1c96ae1893c86382e3bfe1f6d4d3
davialvarenga <davialvarenga@digix.com.br>
2019-05-06 08:52:19 -0400

    Criando critério de idade de prentendente;

---
+++ DesafioGanhadoresDaCasaPopular/Domínio/CriterioDePretendenteComIdadeMaiorQue44Anos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioGanhadoresDaCasaPopular.Domínio
+{
+    class CriterioDePretendenteComIdadeMaiorQue44Anos : ICriterioDePontuacao
+    {
+        public int ObterPontuacao(Familia familia)
+        {
+            var dateTimeIdade = DateTime.Now - familia.ObterPessoaPretendente().DataDeNascimento;
+
+            if (dateTimeIdade.Days / 365 > 44)
+                return 3;
+
+            return 0;
+        }
+    }
+}
---
+++ DesafioGanhadoresDaCasaPopular/Testes/CriterioDePretendenteComIdadeMaiorQue44AnosTeste.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DesafioGanhadoresDaCasaPopular.Builder;
+using DesafioGanhadoresDaCasaPopular.Domínio;
+using NUnit.Framework;
+
+namespace DesafioGanhadoresDaCasaPopular.Testes
+{
+    public class CriterioDePretendenteComIdadeMaiorQue44AnosTeste
+    {
+
+        [Test]
+        public void DeveRetornar3PontosParaPretenteComIdadeMaiorQue44Anos()
+        {
+            var familia = FamiliaBuilder.UmaFamilia()
+                .ComPessoa(PessoaBuilder.UmaPessoa().ComDataDeNascimento(DateTime.Now.AddYears(-45)).Build())
+                .Build();
+            var criterio = new CriterioDePretendenteComIdadeMaiorQue44Anos();
+
+            var pontuacao = criterio.ObterPontuacao(familia);
+
+            Assert.AreEqual(3, pontuacao);
+        }
+
+        [Test]
+        public void NaoDevePontuarParaPretendenteComIdadeMenorQue45Anos()
+        {
+            var familia = FamiliaBuilder.UmaFamilia()
+                .ComPessoa(PessoaBuilder.UmaPessoa().ComDataDeNascimento(DateTime.Now.AddYears(-44)).Build())
+                .Build();
+            var criterio = new CriterioDePretendenteComIdadeMaiorQue44Anos();
+
+            var pontuacao = criterio.ObterPontuacao(familia);
+
+            Assert.AreEqual(0, pontuacao);
+        }
+    }
+}
